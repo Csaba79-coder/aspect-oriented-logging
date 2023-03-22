4b70b49cc9b44b21bc08b2a5e7a3cb428d13f88e
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-22 08:20:09 +0100

    Set up the H2 database in application.properties (to write result in files!) and creat Book entity

---
+++ src/main/java/com/csaba79coder/aspectorientedlogging/book/entity/Book.java
@@ -0,0 +1,29 @@
+package com.csaba79coder.aspectorientedlogging.book.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+}
