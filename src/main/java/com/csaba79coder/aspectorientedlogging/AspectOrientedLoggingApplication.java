9ff869a846f130d46280536adb707c503d174d85
Csaba79-coder <csabavadasz79@gmail.com>
2023-03-22 08:12:45 +0100

    Initial commit

---
+++ src/main/java/com/csaba79coder/aspectorientedlogging/AspectOrientedLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.aspectorientedlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectOrientedLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspectOrientedLoggingApplication.class, args);
+    }
+
+}
